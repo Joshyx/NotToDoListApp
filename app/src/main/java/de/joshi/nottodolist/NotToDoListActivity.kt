9549b125b0d089432e203ccb27266d100f282cdc
Joshua <joshua_boehme@t-online.de>
2022-10-26 20:28:13 +0200

    Added further setup

--- app/src/main/java/de/joshi/nottodolist/NotToDoListActivity.kt
+++ app/src/main/java/de/joshi/nottodolist/NotToDoListActivity.kt
@@ -1,11 +1,20 @@
 package de.joshi.nottodolist
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.graphics.toColorLong
+import java.util.*
 
 class NotToDoListActivity : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,7 +23,17 @@ class NotToDoListActivity : AppCompatActivity() {
         val title: TextView  = findViewById(R.id.mainPageHeading)
 
         createTodoButton.setOnClickListener {
-            title.text = "MOIN"
+            createTodoButton.imageAlpha = 205
+        }
+
+        createTodoButton.setOnTouchListener { view, event ->
+            view.performClick()
+
+            if(event.action == MotionEvent.ACTION_UP) {
+                createTodoButton.imageAlpha = 255
+            }
+
+            true
         }
     }
 }
